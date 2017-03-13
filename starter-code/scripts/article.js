317666ac73d8b6e23aa2b78a7bda84efbb2ff2bc
Martin <martinconradpelayo@gmail.com>
2017-03-13 14:48:44 -0700

    1st driver

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -52,12 +52,22 @@ Article.fetchAll = function() {
     // and then render the index page (using the proper method on the articleView object).
     Article.loadAll(); //TODO: What do we pass in to loadAll()?
     //TODO: What method do we call to render the index page?
+
   } else {
     // TODO: When we don't already have the rawData,
     // we need to retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
     // cache it in localStorage so we can skip the server call next time,
     // then load all the data into Article.all with the .loadAll function above,
     // and then render the index page.
-
+    $.ajax({
+      url:'data/hackerIpsum.json',
+      method: 'GET',
+      success: function(rawData){
+        Article.loadAll(rawData);
+        console.log('first success funciton', rawData),
+        localStorage.setItem('stored_files',rawData);
+        console.log(rawData);
+      }
+    })
   }
 }
